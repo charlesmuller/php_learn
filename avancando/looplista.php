121989e2d349914f18d3b2a0ec06acba3d0995fe
charlesmuller <charlesmuller@rede.ulbra.br>
2022-04-12 21:09:52 -0300

    update looplista.php
    inserindo comentários e ajustes na lógica

--- avancando/looplista.php
+++ avancando/looplista.php
@@ -1,17 +1,22 @@
 <?php
 
-$idadeList = [21, 20, 19, 25, 30, 18, 42];
-$aux = 0;
+$idadeList = [21, 20, 19, 25, 30, 18, 42, 50, 77];
+
+## Mostra lista do fim para o início
 echo "Lista Decrescente" . PHP_EOL;
-for ($i = 6; $i > 0; $i--){
+for ($i = 8; $i > 0; $i--){
     echo $idadeList[$i]. PHP_EOL;
 }
+
+## Mostra lista do inicio para o fim
 echo "Lista Crescente" . PHP_EOL;
-for ($i = 0; $i <= 6; $i++){
+for ($i = 0; $i < count($idadeList); $i++){
     echo $idadeList[$i] . PHP_EOL;
 }
+
+## Mostra as idade com números impares
 echo "Idades Impares" . PHP_EOL;
-for ($i = 0; $i <= 6; $i++){
+for ($i = 0; $i < count($idadeList); $i++){
     $aux = $idadeList[$i];
 
     if($aux % 2 != 0){
@@ -19,8 +24,9 @@ for ($i = 0; $i <= 6; $i++){
     }
 }
 
+## Mostra as idade com números pares
 echo "Idades Pares" . PHP_EOL;
-for ($i = 0; $i <= 6; $i++){
+for ($i = 0; $i < count($idadeList); $i++){
     $aux = $idadeList[$i];
 
     if($aux % 2 == 0){
