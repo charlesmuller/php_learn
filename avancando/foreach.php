9f212b13c6d1586e8018833274d3928276c1fb5c
charlesmuller <charlesmuller@rede.ulbra.br>
2022-04-12 21:50:36 -0300

    update foreach.php
    adicionando um item ao array

--- avancando/foreach.php
+++ avancando/foreach.php
@@ -1,21 +1,28 @@
 <?php
 
 $contasCorrentes = [
-    12345678910 => [
+    '123.456.789-10' => [
         'titular' => 'Charles', 
         'saldo' => 1000
     ], 
-    12340078911 => [
+    '123.400.789-11' => [
         'titular' => 'Maria',
         'saldo' => 10000
     ], 
-    12555678988 => [
+    '125.556.789-88' => [
         'titular' => 'João',
         'saldo' => 250
     ]
 ];
+
+$contasCorrentes['123.999.000-80'] = [
+    'titular' => 'José',
+    'saldo' => 500
+];
+
+
 ## para imprimir os indices que são os CPF dessas contas
 ## acima é feito o foreach para pode buscar dentro do array
 foreach ($contasCorrentes as $cpf => $conta ){
-    echo $cpf . PHP_EOL;
+    echo $cpf . " " . $conta['titular'] . PHP_EOL;
 }
