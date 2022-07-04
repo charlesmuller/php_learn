3c0064929e414fdcb8fcd6832adfa6c6b91713fe
charlesmuller <charlesmuller@rede.ulbra.br>
2022-07-04 20:40:37 -0300

    teste 1

--- avancando/banco.php
+++ avancando/banco.php
@@ -1,19 +1,8 @@
 <?php
 ## exibeMensagem() é a criação de uma subrotina
 
-function exibeMensagem($mensagem)
-{
+function exibeMensagem($mensagem){
     echo $mensagem . PHP_EOL;
-}
-
-function sacar($conta, $valorASacar)
-{
-    if($valorASacar > $conta['saldo']) {
-        exibeMensagem("Você não pode sacar");
-    }else{
-        $conta['saldo'] -= $valorASacar;
-    }
-    return $conta;
 }
 
 $contasCorrentes = [
@@ -30,20 +19,25 @@ $contasCorrentes = [
         'saldo' => 100
     ]
 ];
+if(500 > $contasCorrentes ['123.456.789-10']['saldo']){
+    exibeMensagem("Você não pode sacar");
+}else{
+    $contasCorrentes ['123.456.789-10']['saldo'] -= 500;
+}
 
-<<<<<<< HEAD
 if(500 > $contasCorrentes ['123.400.789-11']['saldo']){
     exibeMensagem("Você não pode sacar");
 }else{
     $contasCorrentes ['123.400.789-11']['saldo'] -= 500;
 }
-=======
-$contasCorrentes['123.456.789-10'] = sacar($contasCorrentes['123.456.789-10'], 500);
-$contasCorrentes['123.400.789-11'] = sacar($contasCorrentes['123.400.789-11'], 500);
-
->>>>>>> f2b3b47d5fa53ef705b37f25a8b823a5430e7386
 ## para imprimir os indices que são os CPF dessas contas
 ## acima é feito o foreach para pode buscar dentro do array
 foreach ($contasCorrentes as $cpf => $conta ){
     echo $cpf . ' ' . $conta['titular'] . ' ' . $conta['saldo'] . PHP_EOL;
 }
+
+
+
+
+
+echo "teste";
