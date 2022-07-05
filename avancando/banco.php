5134150ebef2fc7f8f40fb599f51b89b08255768
charlesmuller <charlesmuller@rede.ulbra.br>
2022-07-05 18:04:19 -0300

    Finalizando

--- avancando/banco.php
+++ avancando/banco.php
@@ -23,15 +23,32 @@ $contasCorrentes['123.456.789-10'] = sacar($contasCorrentes['123.456.789-10'], 2
 
 $contasCorrentes['123.400.789-11'] = sacar($contasCorrentes['123.400.789-11'], 200);
 
-titularLetrasMaiusculas($contasCorrentes['125.556.789-88']);
-
 unset($contasCorrentes['123.400.789-11']);
 
-## para imprimir os indices que são os CPF dessas contas
-## acima é feito o foreach para pode buscar dentro do array
-foreach ($contasCorrentes as $cpf => $conta ){
-    ['titular' => $titular, 'saldo' => $saldo] = $conta;
-    exibeMensagem(
-        "$cpf $titular $saldo"
-    );
-}
+titularLetrasMaiusculas($contasCorrentes['125.556.789-88']);
+?>
+
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Contas Correntes</h1>
+
+    <dl>
+        <?php foreach ($contasCorrentes as $cpf => $conta) { ?>
+        <dt>
+            <h3><?=$conta['titular'] ?> - <?= $cpf; ?></h3>
+        </dt>
+        <dd>
+            Saldo: <?= $conta['saldo']; ?>
+        </dd>
+        <?php } ?>
+    </dl>
+</body>
+</html>
