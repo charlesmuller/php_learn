bb13e148514f2c8676777491487fad9c17914488
charlesmuller <charlesmuller@rede.ulbra.br>
2022-07-11 20:06:58 -0300

    percorrendo arrays

--- manipulando_colecoes_arrays/arrays.php
+++ manipulando_colecoes_arrays/arrays.php
@@ -8,3 +8,12 @@ $array = [
 foreach ($array as $numeral => $nomeNumero) {
     echo "$numeral em português por escrito é: $nomeNumero" . PHP_EOL;
 }
+
+$contador = 0;
+foreach ($array as $numeral => $nomeNumero) {
+    $contador ++;
+}
+
+echo "Total: $contador" . PHP_EOL;
+echo "Total: " . count($array) . PHP_EOL;
+echo "Total: " . sizeof($array) . PHP_EOL;
