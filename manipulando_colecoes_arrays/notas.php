50f58a1ad64012340dc12c26a284b9b94c562808
charlesmuller <charlesmuller@rede.ulbra.br>
2022-07-12 20:25:54 -0300

    ajustando notas.php

--- manipulando_colecoes_arrays/notas.php
+++ manipulando_colecoes_arrays/notas.php
@@ -4,12 +4,19 @@ $notas = [
     'João' => 8,
     'Maria' => 9,
     'Roberto' => 7,
-    'Vinicius' => 6
+    'Vinicius' => null,
 ];
 //do maior para o menor
 //arsort($notas);
-//arsort() mantem as chaves quando vai ordenar
-//ksort() ordena pela ordem das chaves e o krsort para descrescente
-//var_dump($notas);
 
-var_dump(array_is_list($notas));
+
+//arsort($notas)  //mantem as chaves quando vai ordenar
+//ksort($notas) //ordena pela ordem das chaves e o krsort para descrescente
+var_dump($notas);
+
+var_dump(array_is_list($notas)); // retorna true ou false informando se a variável é array
+
+var_dump(array_key_exists('Vinicius', $notas)); // retorna true ou false informando se a key existe no array
+
+var_dump(isset($notas['Vinicius'])); // retorna true ou false informando se o elemento tem valor setado
+
