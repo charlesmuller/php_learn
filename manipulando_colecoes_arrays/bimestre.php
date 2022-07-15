b74e1f58090e29b31ad9f089ec93c642bb7e48e0
charlesmuller <charlesmuller@rede.ulbra.br>
2022-07-15 18:17:16 -0300

    atualizando

--- manipulando_colecoes_arrays/bimestre.php
+++ manipulando_colecoes_arrays/bimestre.php
@@ -14,15 +14,16 @@ $notasBimestre2 = [
     'Roberto' => 7,
 ];
 
-var_dump(array_diff($notasBimestre1, $notasBimestre2)); //retorna um novo array com todas as notas/valores que estão no bimestre 1 que não estão no bimestre 2
+//var_dump(array_diff($notasBimestre1, $notasBimestre2)); //retorna um novo array com todas as notas/valores que estão no bimestre 1 que não estão no bimestre 2
 
-var_dump(array_diff_key($notasBimestre1, $notasBimestre2)); //retorna um novo array com todas as chaves que estão no bimestre 1 que não estão no bimestre 2
-
-var_dump(array_diff_assoc($notasBimestre1, $notasBimestre2)); //compara os dois arrays e retorna os elementos (tanto chave quanto valor) e retorna quem não está na no primeiro array
+//var_dump(array_diff_key($notasBimestre1, $notasBimestre2)); //retorna um novo array com todas as chaves que estão no bimestre 1 que não estão no bimestre 2
+//
+//var_dump(array_diff_assoc($notasBimestre1, $notasBimestre2)); //compara os dois arrays e retorna os elementos (tanto chave quanto valor) e retorna quem não está no primeiro array
+//
 
 $alunosFaltantes = array_diff_key($notasBimestre1, $notasBimestre2); //compara os dois arrays e joga na variável
 $nomesAlunos = array_keys($alunosFaltantes);
 $notasAlunos = array_values($alunosFaltantes);
 
 var_dump(array_combine($notasAlunos, $nomesAlunos)); //Troca as chaves do array pelos valores dos arrays
-var_dump(array_flip($alunosFaltantes)); // Faz a mesma coisa do de cima
+//var_dump(array_flip($alunosFaltantes)); // Faz a mesma coisa do de cima
