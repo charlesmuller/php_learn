6a317439a2a03dcc203495a2d31de1bd2fb22b08
charlesmuller <charlesmuller@rede.ulbra.br>
2022-07-15 17:05:33 -0300

    inserção no array

--- manipulando_colecoes_arrays/matriculas.php
+++ manipulando_colecoes_arrays/matriculas.php
@@ -15,4 +15,9 @@ $novosAlunos = [
     'Daiane',
 ];
 
-var_dump($alunos2022 = array_merge($alunos2022, $novosAlunos));
+//$alunos2022 = array_merge($alunos2022, $novosAlunos);
+$alunos2022 = [...$alunos2022, "Teste de inserção", ...$novosAlunos];
+
+var_dump($alunos2022);
+
+
